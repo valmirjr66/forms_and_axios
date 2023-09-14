c999436988227ff73ffe8d4ae191f68181621b32
Valmir Gonçalves Martins Júnior <valmir.junior.external@bt.com>
2023-09-14 18:09:38 -0300

    Implement title animation

--- front/src/components/Panel/index.style.js
+++ front/src/components/Panel/index.style.js
@@ -7,7 +7,42 @@ export const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
-  background-color: darkslategray;
-  color: white;
   flex-direction: column;
 `;
+
+export const AnimatedTitle = {
+  Box: styled.div`
+  width: 400px;
+  height: 200px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 60px;
+  font-family: Coffee;
+  flex-grow: 1;
+  cursor: pointer;
+
+  ${(props) => props.play &&
+    `span {
+      animation: breeze ${props.duration || 1000}ms ease;
+      animation-delay: calc(50ms * var(--i));
+    }`
+  }
+  
+  @keyframes breeze {
+    0%, 100% {
+      transform: scale(1);
+    }
+
+    50% {
+      transform: scale(1.2) translateY(-10px);
+      filter: blur(5px);
+      opacity: 0.2
+    }
+  }
+  `,
+  Fragment: styled.span`
+    padding: 0px 2px;
+  `
+}

fc8091c0b6337b2cbf34dd469fc615f77f1e8366
Rafael Barbosa da Silva <73250106+RafaSilvaDev@users.noreply.github.com>
2023-11-09 10:02:13 -0300

    404 page implemented (#17)
    404 page implemented with lottie animation.

---
+++ front/src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Panel from "../components/Panel";
+import NotFoundView from "../components/404";
+
+export default function NotFound() {
+  return (
+    <Panel>
+      <NotFoundView />
+    </Panel>
+  );
+}
